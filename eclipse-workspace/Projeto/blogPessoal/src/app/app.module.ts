fc15dbd11bec1ef1987fe1d893d575496344f376
VanessaAntonio0102 <vanessa.antonio0102@gmail.com>
2022-06-15 21:32:14 -0300

    ultimas atulizações

--- eclipse-workspace/Projeto/blogPessoal/src/app/app.module.ts
+++ eclipse-workspace/Projeto/blogPessoal/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,6 +8,9 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { FormsModule } from '@angular/forms';
+import { InicioComponent } from './inicio/inicio.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,13 +18,21 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy 
+  }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
